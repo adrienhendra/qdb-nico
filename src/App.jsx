4a9ffee987ece187a2e11bdb81a1b8acfe168bbe
adrienhs <adrienhs@gmail.com>
2018-05-02 15:39:02 +0700

    Add firebase and react-quill

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+
 class App extends Component {
   render() {
     return (
